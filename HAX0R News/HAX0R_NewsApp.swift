0908e632b1a06cb704a347f4e854b99d9b7d16c8
Denis <shiirochi@yandex.ru>
2021-02-11 23:13:31 +0300

    Initial Commit

---
+++ HAX0R News/HAX0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HAX0R_NewsApp.swift
+//  HAX0R News
+//
+//  Created by Денис Богданенко on 11.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct HAX0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
